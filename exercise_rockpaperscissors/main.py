0212ab640fb5a7d385a065c28cccd600e2e9a26f
DubStray <francesco.gaming00@gmail.com>
2024-05-07 23:04:10 +0200

    prova

--- exercise_rockpaperscissors/main.py
+++ exercise_rockpaperscissors/main.py
@@ -1,46 +1,55 @@
 import random
-print ("Welcome to the Rock - Paper - Scissors game!\n")
+print("Welcome to the Rock - Paper - Scissors game!\n")
+
 rps = ["Rock", "Paper", "Scissors"]
-lives = 3
+lifes = 3
 game_is_running = True
+core_game = True
 
-while lives > 0:
-    decisione_giocatore = input("What you choose? Rock, Paper or scissors? ")
-    decisione_cpu = rps[random.randint(0, 2)]
-    if decisione_giocatore.lower() == "rock":
-        if decisione_cpu == "Rock":
-            print("The CPU choose 'Rock', it's a draw!")
-            print(f"You have {lives} lives left")
-        elif decisione_cpu == "Paper":
-            print("The CPU choose 'Paper', you lose!")
-            lives -= 1
-            print(f"You have {lives} lives left")
-        else:
-            print("The CPU choose 'Scissors', you win!")
-            print(f"You have {lives} lives left")
-    if decisione_giocatore.lower() == "paper":
-        if decisione_cpu == "Rock":
-            print("The CPU choose 'Rock', you win!")
-            print(f"You have {lives} lives left")
-        elif decisione_cpu == "Paper":
-            print("The CPU choose 'Paper', it's a draw!")
-            print(f"You have {lives} lives left")
-        else:
-            print("The CPU choose 'Scissors', you lose!")
-            lives -= 1
-            print(f"You still have {lives} lives left")
-    if decisione_giocatore.lower() == "scissors":
-        if decisione_cpu == "Rock":
-            print("The CPU choose 'Rock', you lose!")
-            lives -= 1
-            print(f"You have {lives} lives left")
-        elif decisione_cpu == "Paper":
-            print("The CPU choose 'Paper', you win!")
-            print(f"You have {lives} lives left")
-        else:
-            print("The CPU choose 'Scissors', it's a draw!")
-            print(f"You have {lives} lives left")
-
-if lives == 0:
-    print("You have run out of lives, game over.")
-    game_is_running = False
+while core_game == True:
+    while lifes > 0:
+        decisione_giocatore = input("What you choose? Rock, Paper or scissors? ")
+        decisione_cpu = rps[random.randint(0, 2)]
+        if decisione_giocatore.lower() == "rock":
+            if decisione_cpu == "Rock":
+                print("The CPU choose 'Rock', it's a draw!")
+                print(f"You have {lifes} lives left")
+            elif decisione_cpu == "Paper":
+                print("The CPU choose 'Paper', you lose!")
+                lifes -= 1
+                print(f"You have {lifes} lives left")
+            else:
+                print("The CPU choose 'Scissors', you win!")
+                print(f"You have {lifes} lives left")
+        if decisione_giocatore.lower() == "paper":
+            if decisione_cpu == "Rock":
+                print("The CPU choose 'Rock', you win!")
+                print(f"You have {lifes} lives left")
+            elif decisione_cpu == "Paper":
+                print("The CPU choose 'Paper', it's a draw!")
+                print(f"You have {lifes} lives left")
+            else:
+                print("The CPU choose 'Scissors', you lose!")
+                lifes -= 1
+                print(f"You have {lifes} lives left")
+        if decisione_giocatore.lower() == "scissors":
+            if decisione_cpu == "Rock":
+                print("The CPU choose 'Rock', you lose!")
+                lifes -= 1
+                print(f"You have {lifes} lives left")
+            elif decisione_cpu == "Paper":
+                print("The CPU choose 'Paper', you win!")
+                print(f"You have {lifes} lives left")
+            else:
+                print("The CPU choose 'Scissors', it's a draw!")
+                print(f"You have {lifes} lives left")
+    print("You have run out of lifes, game over")
+    game_restart = input("Wanna play again? Y/N: ").lower()
+    if game_restart == "y":
+        core_game = True
+        lifes += 3
+    elif game_restart == "n":
+        core_game = False
+        game_is_running = False
+        print("See you next time!")
+        break
