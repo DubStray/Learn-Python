3e8046e7d604c75dac93773d9219dc5f1242f051
DubStray <francesco.gaming00@gmail.com>
2024-05-10 11:26:43 +0200

    modifiche

--- exercise_hangman/main.py
+++ exercise_hangman/main.py
@@ -1,12 +1,19 @@
 import random
 word_list = ["paper", "rock", "scissors", "bread", "gold", "silver", "human", "monster"]
+display = []
 print("Welcome to the Hangman game!")
 
 chosen_word = random.choice(word_list)
 guess = input("Guess a letter: ").lower()
+print(f"Pssst, you word is {chosen_word}")
 
-for letter in chosen_word:
+word_lenght = len(chosen_word)
+
+for letter in range(len(chosen_word)):
+    display += "_"
+print(display)
+
+for position in range(0, len(chosen_word)):
+    letter = chosen_word(position)
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
