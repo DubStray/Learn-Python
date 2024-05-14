a460a49baf6083bbd46c3582218d40a75fd17a1a
DubStray <francesco.gaming00@gmail.com>
2024-05-14 20:11:42 +0200

    modifiche caesar cipher exercise

--- exercise_caesar_cipher/main.py
+++ exercise_caesar_cipher/main.py
@@ -9,6 +9,7 @@ def caesar(start_text, shift_amount, cipher_direction):
     final_text = "" # Si crea una variabile di testo vuota da riempire col messaggio.
     if cipher_direction == "decode": 
         shift_amount *= -1 # Il '*=' sta a significare 'moltiplica' quindi il numero dell'input moltiplicato per -1 sarà sempre lo stesso numero ma in negativo.
+        # Immagina questo sopra come: 5 * -1 = -5
     for letter in start_text: # start_text prende l'input di di 'text'
         position = alphabet.index(letter) # Variabile che utilizza il .index() per trovare l'indice della lettera.
         new_position = position + shift_amount # Quindi nella posizione attuale della lettera si aggiungerà lo 'shift' dell'input, e quindi verrà 'shiftata' di tot numeri.
