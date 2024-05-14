6cc52752e606ea65bccd25d5306498eebc6347cf
DubStray <francesco.gaming00@gmail.com>
2024-05-14 20:10:25 +0200

    modifiche caesar cipher exercise

--- exercise_caesar_cipher/main.py
+++ exercise_caesar_cipher/main.py
@@ -4,30 +4,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction.lower() == "encode":
+# Blocco con una funzione per far partire il programma.
+def caesar(start_text, shift_amount, cipher_direction):
+    final_text = "" # Si crea una variabile di testo vuota da riempire col messaggio.
+    if cipher_direction == "decode": 
+        shift_amount *= -1 # Il '*=' sta a significare 'moltiplica' quindi il numero dell'input moltiplicato per -1 sarà sempre lo stesso numero ma in negativo.
+    for letter in start_text: # start_text prende l'input di di 'text'
+        position = alphabet.index(letter) # Variabile che utilizza il .index() per trovare l'indice della lettera.
+        new_position = position + shift_amount # Quindi nella posizione attuale della lettera si aggiungerà lo 'shift' dell'input, e quindi verrà 'shiftata' di tot numeri.
+        final_text += alphabet[new_position] # Si riempie quindi il final_text con le lettere shiftate.
+    print(f"Here's the {direction}d result: {final_text}")
 
-    # Blocco per creare un messaggio criptato.
-    def encrypt(plain_text, shift_amount): # Creo una funzione che riprende i comandi 'text' e 'shift'.
-        cipher_text = ""
-        for letter in plain_text:
-            position = alphabet.index(letter) # Con .index() vado a trovare l'indice della lettera e il suo numero.
-            new_position = position + shift_amount # In questo modo si fa l'addizione tra la posizione della lettera e la 'shifta' di un numero dato dall'input.
-            new_letter = alphabet[new_position]
-            cipher_text += new_letter
-        print(f"The encoded text is {cipher_text}")
-
-    encrypt(plain_text=text, shift_amount=shift)
-
-else:
-
-    # Blocco per decrittare un messaggio criptato.
-    def decrypt(plain_text, shift_amount):
-        cipher_text = ""
-        for letter in plain_text:
-            position = alphabet.index(letter)
-            new_position = position - shift_amount # La posizione delle lettere devono tornare normali, quindi al contrario si usa il -
-            new_letter = alphabet[new_position]
-            cipher_text += new_letter
-        print(f"The decrypted text is {cipher_text}")
-
-    decrypt(plain_text=text, shift_amount=shift)
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
