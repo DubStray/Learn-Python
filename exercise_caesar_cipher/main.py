e362e8ab66cd295ed9e85bc9c8b444a7a50e78fc
DubStray <francesco.gaming00@gmail.com>
2024-05-15 16:23:10 +0200

    Caesar Cipher completato

--- exercise_caesar_cipher/main.py
+++ exercise_caesar_cipher/main.py
@@ -1,8 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+from art import logo
+print(logo)
+
 
 # Blocco con una funzione per far partire il programma.
 def caesar(start_text, shift_amount, cipher_direction):
@@ -10,10 +11,28 @@ def caesar(start_text, shift_amount, cipher_direction):
     if cipher_direction == "decode": 
         shift_amount *= -1 # Il '*=' sta a significare 'moltiplica' quindi il numero dell'input moltiplicato per -1 sarà sempre lo stesso numero ma in negativo.
         # Immagina questo sopra come: 5 * -1 = -5
-    for letter in start_text: # start_text prende l'input di di 'text'
-        position = alphabet.index(letter) # Variabile che utilizza il .index() per trovare l'indice della lettera.
-        new_position = position + shift_amount # Quindi nella posizione attuale della lettera si aggiungerà lo 'shift' dell'input, e quindi verrà 'shiftata' di tot numeri.
-        final_text += alphabet[new_position] # Si riempie quindi il final_text con le lettere shiftate.
+    for char in start_text: # start_text prende l'input di di 'text'
+        if char in alphabet:
+            position = alphabet.index(char) # Variabile che utilizza il .index() per trovare l'indice della lettera.
+            new_position = position + shift_amount # Quindi nella posizione attuale della lettera si aggiungerà lo 'shift' dell'input, e quindi verrà 'shiftata' di tot numeri.
+            final_text += alphabet[new_position] # Si riempie quindi il final_text con le lettere shiftate.
+        else:
+            final_text += char
     print(f"Here's the {direction}d result: {final_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+code_ending = False
+while not code_ending:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+    restart = input("You want to use the Caesar Cipher again? Y/N ")
+    if restart == "n":
+        code_ending = True
+        print("Goodbye.")
