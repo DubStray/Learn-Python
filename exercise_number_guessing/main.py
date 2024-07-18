c7a176b0a6f2c87a152e2ad7505ad893c7c7a9de
DubStray <francesco.gaming00@gmail.com>
2024-07-18 15:58:33 +0200

    aggiunti commenti

--- exercise_number_guessing/main.py
+++ exercise_number_guessing/main.py
@@ -1,12 +1,12 @@
 import random
 from art import logo
 
-def choose_number():
-    global randnumber
-    randnumber = random.randint(1, 100)   
+def choose_number():                            # Funzione con lo scopo di scegliere un numero randomico da 1 a 100
+    global randnumber                           # Rendendo globale la variabile randnumer per poterla richiamare
+    randnumber = random.randint(1, 100)         # alrove nel codice.
 
 
-def play_game():
+def play_game():                                # Funzione principale.
 
     is_game_over = False
     
@@ -14,22 +14,22 @@ def play_game():
     print("Welcome to the number guessing game!")
     difficulty = input("Choose the difficulty: Easy (10 guesses) or Hard (5 Guesses): ").lower()
 
-    global lives
+    global lives                                # Variabile "lives" resa globale
     
     if difficulty == "easy":
-        lives = 10
+        lives = 10                              # Sistema di vite in base alla difficoltà
     if difficulty == "hard":
         lives = 5
 
-    choose_number()
+    choose_number()                             
 
     while not is_game_over:
         
         player_choose = int(input("Try to guess the number from 1 to 100!  Number: "))
 
         if player_choose > randnumber:
-            print("Too high")
-            lives -= 1
+            print("Too high")                   # Confronto tra la risposta dell'utente e il numero
+            lives -= 1                          # scelto dal computer
             print(f"You have {lives} guesses left!")
 
         elif player_choose < randnumber:
