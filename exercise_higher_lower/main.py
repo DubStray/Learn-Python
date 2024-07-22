0bca548b5f058543702d8f9e8645088df36c70db
DubStray <francesco.gaming00@gmail.com>
2024-07-22 17:18:16 +0200

    new exercise

---
+++ exercise_higher_lower/main.py
@@ -0,0 +1,11 @@
+import random
+from game_data import data
+from art import logo, vs
+
+def choosing_cards(data):
+    my_data = data
+    for value in my_data:
+        print(mydata[name, follower_count, description, country])
+        
+
+choosing_cards(data)
