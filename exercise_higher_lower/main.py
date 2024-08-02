2ce0442e9cf62b6daa1739ae0af66ba6720b52cc
DubStray <francesco.gaming00@gmail.com>
2024-08-02 23:27:41 +0200

    fixes

--- exercise_higher_lower/main.py
+++ exercise_higher_lower/main.py
@@ -33,23 +33,23 @@ def game():
 
     score = 0
     game_should_continue = True
-    item_A = generate_random_item()                             # Inizia il gioco prendendo due item randomici
+    item_A = generate_random_item()                                         # Inizia il gioco prendendo due item randomici
     item_B = generate_random_item()
 
     while game_should_continue:                             
-        item_A = item_B                                         # Questa parte di While serve nel caso il player indovini la risposta
-        item_B = generate_random_item()                         # in quel caso se la risposta giusta era la B, la B diventerà la A
+        item_A = item_B                                                     # Questa parte di While serve nel caso il player indovini la risposta
+        item_B = generate_random_item()                                     # in quel caso se la risposta giusta era la B, la B diventerà la A
 
-        while item_A == item_B:                                 # Mentre l'item B diventa l'item A, l'item B viene generato nuovamente
+        while item_A == item_B:                                             # Mentre l'item B diventa l'item A, l'item B viene generato nuovamente
             item_B = generate_random_item()
 
-        print(f"Compare A: {format_data(item_A)}.")             # Formatta i dati di ogni item e chiede di compararli
+        print(f"Compare A: {format_data(item_A)}.")                         # Formatta i dati di ogni item e chiede di compararli
         print(vs)
         print(f"Against B: {format_data(item_B)}.")
 
         guess = input("Who has more follower? A or B?  ").lower()
-        a_follower_count = item_A["follower_count"]             # Segna (non a schermo) i follower count di ogni item per capire chi dei due
-        b_follower_count = item_B["follower_count"]             # ha più follower
+        a_follower_count = item_A["follower_count"]                         # Segna (non a schermo) i follower count di ogni item per capire chi dei due
+        b_follower_count = item_B["follower_count"]                         # ha più follower
         is_correct = check_answer(guess, a_follower_count, b_follower_count)    # Farà un check della risposta tra l'input (guess) e i follower di ognuno (A e B)
 
         clear()
