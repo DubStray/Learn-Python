cef8ccd5b931d0c1e9a35e501e7e3de9c0b85701
DubStray <francesco.gaming00@gmail.com>
2024-09-09 17:31:12 +0200

    modifiche

--- exercise_coffee_machine/main.py
+++ exercise_coffee_machine/main.py
@@ -33,3 +33,35 @@ resources = {
 
 money = 0
 
+def splash_screen():
+    '''Fa vedere la welcome page della macchinetta del caffè con tutte le opzioni disponibili'''
+
+    print('''
+        Welcome to the coffee machine!
+        
+        ----MENU----
+        Espresso: 1.50$
+        Latte: 2.50$
+        Cappuccino: 3.00$
+        ------------
+        
+        Type 'report' to check the resources
+        Type 'off' to log out from the machine
+        ''')
+
+def show_report():
+    '''Opzione di report che permette di mostrare le resources disponibili della macchinetta'''
+
+    print("Resources: ")
+    for k in resources:
+        print(k)
+        print(resources[k])
+
+def main():
+    splash_screen()
+
+    user_choose = input("        Type here: ")
+    if user_choose.lower() == "report":
+        show_report()
+    if user_choose.lower() == "off":
+        # Rompere il loop
